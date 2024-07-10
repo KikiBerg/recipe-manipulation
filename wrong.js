31a3b70a6b40ef52a6a579b2c5ca190b9918aeb3
KikiBerg <153670000+KikiBerg@users.noreply.github.com>
2024-07-10 11:45:39 +0200

    Further fixing

--- wrong.js
+++ wrong.js
@@ -17,13 +17,22 @@ document.querySelector('.image-container img').src =
 document.querySelector('.ingredients-container').style.backgroundColor =
   '#f9f9f9';
 
-const bottomIngredientsList = document.querySelector('.ingredients-list-bottom');
-bottomIngredientsList.innerHTML = '<li>15st digistivetex</li><li>Lite smör</li>';
+const bottomIngredientsList = document.querySelector(
+  '.ingredients-list-bottom'
+);
+bottomIngredientsList.innerHTML =
+  '<li>15st digistivetex</li><li>Lite smör</li>';
 
-document.querySelector('.ingredients-list-paste li:nth-child(3)').textContent = '3tsk vaniljsocker';
+document.querySelector('.ingredients-list-paste li:nth-child(3)').textContent =
+  '3tsk vaniljsocker';
 
 const pasteIngredientsList = document.querySelector('.ingredients-list-paste');
 const newIngredient = document.createElement('li');
 newIngredient.textContent = '400g naturell philadelphiaost';
 pasteIngredientsList.appendChild(newIngredient);
 
+document.querySelector('.instructions').classList.remove('shadow');
+
+const instructionsList = document.querySelector('.instructions-list');
+instructionsList.children[1].textContent = 'Separera ägggulor och äggvitor. Äggvitorna lägger du i en stor bunke, äggulorna i en liten bunke.';
+instructionsList.children[8].textContent = 'Ställ in i frysen över natten.';
