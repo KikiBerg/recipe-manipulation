61057f0e21aafaa4e050b46583b81dded5b427bf
KikiBerg <153670000+KikiBerg@users.noreply.github.com>
2024-07-10 11:02:01 +0200

    Fourth ingredient, instructions array

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ console.log('Recipe name: ', recipeName);
 const recipeNameTag = document.getElementById('recipe-name').tagName;
 console.log('Recipe name tag: ', recipeNameTag);
 
-const descriptionFont = window.getComputedStyle(document.querySelector('.description')).fontSize
+const descriptionFont = window.getComputedStyle(
+  document.querySelector('.description')
+).fontSize;
 console.log('Description font size: ', descriptionFont);
 
 const imageAlt = document.querySelector('.image-container img').alt;
@@ -14,9 +16,22 @@ const image = document.querySelector('.image-container img');
 const imageInfo = {
   url: image.src,
   height: image.naturalHeight,
-  width: image.naturalWidth
+  width: image.naturalWidth,
 };
 console.log('Image info:', imageInfo);
 
-const pasteIngredients = document.querySelector('.ingredients-list-paste').children.length;
+const pasteIngredients = document.querySelector('.ingredients-list-paste')
+  .children.length;
 console.log('Number of paste ingredients: ', pasteIngredients);
+
+const fourthIngredient = document.querySelector('.ingredients-list-paste')
+  .children[3].textContent;
+console.log('Fourth paste ingredient:', fourthIngredient);
+
+const instructionsList = document.querySelector('.instructions-list');
+const instructions = Array.from(instructionsList.children).map((li, index) => ({
+  order: index + 1,
+  text: li.textContent.trim(),
+}));
+
+console.log('Instructions:', instructions);
